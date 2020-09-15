465781675f822d3eef9285a7c7d1c45b420fc2c1
Vivek Sanghvi <viksipart@gmail.com>
2020-09-15 22:15:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,11 +13,13 @@ end
 
 def selection(num)
   if num = 1
+    
      "YUM YUM MUNCH MUNCH MUNCH"
-     binding.pry
+     
   elsif num = 2
+  binding.pry
      "HAM HAM HAM IN MY TUMMY"
-     binding.pry
+     
   end
 end
 
